eb689d50e194cceb1badb6cb326bf5b14f99d9d7
Mohammad Baghery <mohammadbaghery7@gmail.com>
2022-11-12 20:43:05 +0330

    Add forming with % homework

--- 1.Ali Test.py
+++ 1.Ali Test.py
@@ -10,3 +10,9 @@ print(_age + "2")
 c='Family'
 c=c.upper()
 print(c)
+
+#Forming with %
+name='test'
+price=1.23456
+_text='Hello %s name and price is %4.4f and it is good' %(name, price)
+print(_text)
