739278bc1d83adb503ac0c9913cb67454e8beead
Mohammad Baghery <mohammadbaghery7@gmail.com>
2022-11-12 21:20:57 +0330

    Add Slice example

--- 1.Ali Test.py
+++ 1.Ali Test.py
@@ -39,3 +39,7 @@ print("UserAges 2 = ",_userages2)
 #Adding part of a list
 _userages3=_userages[2:4]
 print("UserAges 3 = ",_userages3)
+
+#Slice example
+_userages4=_userages[1:5:2]
+print("UserAges 4 = ",_userages4)
