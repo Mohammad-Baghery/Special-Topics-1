21125a1e388c171c9bdcbd82e04356413e5c6cf8
Mohammad Baghery <mohammadbaghery7@gmail.com>
2022-11-28 10:16:11 +0330

    Added 3rd Session HomeWorks

---
+++ 2.3rd Session.py
@@ -0,0 +1,6 @@
+num1=int(10)
+num2=int(5)
+print("{}=={1}:{2}".format(num1,num2,num1==num2))
+print("{}!={1}:{2}".format(num1,num2,num1==num2))
+print("{}<>{1}:{2}".format(num1,num2,num1==num2))
+print("{}<{1}:{2}".format(num1,num2,num1==num2))
