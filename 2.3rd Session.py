c205ff52c66f8ed2ee183b02e535427463c0cbb6
Mohammad Baghery <mohammadbaghery7@gmail.com>
2022-11-28 10:27:15 +0330

    Added Shifting Examples

--- 2.3rd Session.py
+++ 2.3rd Session.py
@@ -9,3 +9,14 @@ print("{}<{1}:{2}".format(num1,num2,num1==num2))
 # & examples
 Resault=(5&3)
 print(Resault)
+resault=(5/3)
+print(resault)
+
+#xor Example
+print(5^3)
+
+#Shifting Examples
+x=10<<2
+print(x)
+y=10>>2
+print(y)
