49e6e5ea959ea524ab154ae4471d337ec76cf0d3
Mohammad Baghery <mohammadbaghery7@gmail.com>
2022-11-28 10:22:07 +0330

    Added & Examples

--- 2.3rd Session.py
+++ 2.3rd Session.py
@@ -1,6 +1,11 @@
+#3rd Session
 num1=int(10)
 num2=int(5)
 print("{}=={1}:{2}".format(num1,num2,num1==num2))
 print("{}!={1}:{2}".format(num1,num2,num1==num2))
 print("{}<>{1}:{2}".format(num1,num2,num1==num2))
 print("{}<{1}:{2}".format(num1,num2,num1==num2))
+
+# & examples
+Resault=(5&3)
+print(Resault)
